66b61f0e0e5f206f1277f7f982288ad7435729ea
Ayaahamed73 <121058141+Ayaahamed73@users.noreply.github.com>
2022-12-27 22:01:55 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+class program
+{
+	static void Main(string[] args)
+	{
+
+		Console.WriteLine("please enter n1");
+		int n1 = int.Parse(Console.ReadLine());
+		Console.WriteLine("please enter n2");
+		int n2 = int.Parse(Console.ReadLine());
+		int i, j;
+		for (i = n1; i < n2; i++)
+		{
+
+			for (j = 2; j < n2; j++)
+			{
+
+				if (i != j && i % j == 0)
+				{
+					break;
+				}
+
+
+			}
+			if (i % j != 0 && i > 1)
+				Console.WriteLine(i);
+		}
+
+
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
